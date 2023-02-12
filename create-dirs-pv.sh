2db558c69d37a4653fe46e3c8298e75b28acd03b
Kunal Gupta <assaxor@gmail.com>
2023-02-13 01:35:56 +0530

    feat: add velero with c4 diagram

--- create-dirs-pv.sh
+++ create-dirs-pv.sh
@@ -2,7 +2,7 @@
 
 #use this for creating dirs for required pvs
 
-cmd='for i in $(seq 0 10); do mkdir -p /containers/services/docker/rootfs/tmp/work-pg/node-$i; done && chmod 0777 -R /containers/services/docker/rootfs/tmp/work-pg && ls /containers/services/docker/rootfs/tmp/work-pg/*'
+cmd='base_dir=/containers/services/docker/rootfs/tmp && mkdir -p $base_dir/snapshots && chmod 0777 -R $base_dir/snapshots && mkdir -p /var/velero-local-volume-provider && chmod 0777 -R /var/velero-local-volume-provider && for i in $(seq 0 10); do mkdir -p $base_dir/work-pg/node-$i; done && chmod 0777 -R $base_dir/work-pg && ls $base_dir/work-pg/*'
 
 
 echo "$cmd" | docker run -i --rm --privileged --pid=host justincormack/nsenter1
---
+++ install_csi.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#use this for creating dirs for required pvs
+
+SNAPSHOTTER_VERSION=v6.2.1
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/${SNAPSHOTTER_VERSION}/client/config/crd/snapshot.storage.k8s.io_volumesnapshotclasses.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/${SNAPSHOTTER_VERSION}/client/config/crd/snapshot.storage.k8s.io_volumesnapshotcontents.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/${SNAPSHOTTER_VERSION}/client/config/crd/snapshot.storage.k8s.io_volumesnapshots.yaml
+
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/${SNAPSHOTTER_VERSION}/deploy/kubernetes/snapshot-controller/rbac-snapshot-controller.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/${SNAPSHOTTER_VERSION}/deploy/kubernetes/snapshot-controller/setup-snapshot-controller.yaml
